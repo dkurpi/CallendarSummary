9b89232edcd8733ecb0d4c61d033d4595f7a39ce
Dawid Kurpiel <kurpieldawid@gmail.com>
2021-03-23 23:01:01 +0100

    Made required functionality

--- src/CalendarSummary/index.tsx
+++ src/CalendarSummary/index.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { getCalendarEvents } from '../api-client';
+import React, { useEffect, useState } from "react";
+import { CalendarEvent, getCalendarEvents } from "../api-client";
+import { getDatesObjectFromCurrentDay, convertTolist } from "../helpers/dates";
 
 const CalendarSummary: React.FunctionComponent = () => {
+  const [datesList, setDatesList] = useState<SingleDayObject[]>([]);
+
+  useEffect(() => {
+    const dateObjectList = getDatesObjectFromCurrentDay(7);
+    const dateListPromises: Promise<CalendarEvent[]>[] = dateObjectList.map((date) =>
+      getCalendarEvents(date)
+    );
+
+    Promise.all(dateListPromises).then((data) => {
+      const convertedList = convertTolist(dateObjectList, data);
+      setDatesList(convertedList);
+    });
+  }, []);
+
   return (
     <div>
       <h2>Calendar summary</h2>
-      {/* Not implemented */}
+
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Number of events</th>
+          <th>Total duration [min]</th>
+          <th>Longest event</th>
+        </tr>
+
+        {datesList.map(
+          ({ date, totalDuration, numberOfEvents, longestEvent }) => (
+            <tr>
+              <td>{date}</td>
+              <td>{numberOfEvents}</td>
+              <td>{totalDuration}</td>
+              <td>{longestEvent}</td>
+            </tr>
+          )
+        )}
+      </table>
     </div>
   );
 };
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+
+interface SingleDayObject {
+    date: string;
+    numberOfEvents: number;
+    totalDuration: number;
+    longestEvent: string;
+  }
